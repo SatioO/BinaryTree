0012208019e7c76080098a72067a5b9f161c5920
SatioO <vaibhav.satam29991@gmail.com>
2020-10-20 15:47:18 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,139 +1,140 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BinaryTree {
   constructor() {
-    this.root = null
+    this.root = null;
   }
-  
-  add(node) {
-    if(this.root === null) {
-      this.root = node
+
+  add(value) {
+    const node = new Node(value);
+
+    if (this.root === null) {
+      this.root = node;
       return;
     } else {
       function search(root) {
-          if(node.value > root.value) {
-            if(root.right === null) {
-              root.right = node
-            } else {
-              search(root.right)
-            }
-          } else if(node.value < root.value) {
-            if(root.left === null) {
-              root.left = node
-            } else {
-              search(root.left)
-            }
+        if (node.value > root.value) {
+          if (root.right === null) {
+            root.right = node;
           } else {
-            return null
+            search(root.right);
           }
+        } else if (node.value < root.value) {
+          if (root.left === null) {
+            root.left = node;
+          } else {
+            search(root.left);
+          }
+        } else {
+          return null;
+        }
       }
-       
-      let root = this.root
-      search(root)
+
+      let root = this.root;
+      search(root);
     }
   }
-  
+
   findMin() {
-    let root = this.root
-    
-    while(root.left) {
-      root = root.left
+    let root = this.root;
+
+    while (root.left) {
+      root = root.left;
     }
-    
-    return root.value
+
+    return root.value;
   }
-  
+
   findMax() {
-    let root = this.root
-    
-    while(root.right) {
-      root = root.right
+    let root = this.root;
+
+    while (root.right) {
+      root = root.right;
     }
-    
-    return root.value
+
+    return root.value;
   }
-  
+
   find(value) {
     function search(root) {
-      if(value === root.value) {
-        return root
-      } else if(value < root.value) {
-        return search(root.left)
-      } else if(value > root.value) {
-        return search(root.right)
+      if (value === root.value) {
+        return root;
+      } else if (value < root.value) {
+        return search(root.left);
+      } else if (value > root.value) {
+        return search(root.right);
       } else {
-        return null
+        return null;
       }
     }
-    
-    let root = this.root
-    return search(root)
+
+    let root = this.root;
+    return search(root);
   }
 
   inTraverse() {
-    const result = []
-    
+    const result = [];
+
     function traversal(root) {
-      if(root.left) {
-        traversal(root.left)
+      if (root.left) {
+        traversal(root.left);
       }
 
-      result.push(root.value)
+      result.push(root.value);
 
-      if(root.right) {
-        traversal(root.right)
+      if (root.right) {
+        traversal(root.right);
       }
     }
-    
-    traversal(this.root)
 
-    return result
+    traversal(this.root);
+
+    return result;
   }
 
   preTraverse() {
-    const result = []
-    
+    const result = [];
+
     function traversal(root) {
-      result.push(root.value)
-      if(root.left) {
-        traversal(root.left)
+      result.push(root.value);
+      if (root.left) {
+        traversal(root.left);
       }
 
-      if(root.right) {
-        traversal(root.right)
+      if (root.right) {
+        traversal(root.right);
       }
     }
-    
-    traversal(this.root)
 
-    return result
+    traversal(this.root);
+
+    return result;
   }
 
   postTraverse() {
-    const result = []
-    
+    const result = [];
+
     function traversal(root) {
-      
-      if(root.left) {
-        traversal(root.left)
+      if (root.left) {
+        traversal(root.left);
       }
 
-      if(root.right) {
-        traversal(root.right)
+      if (root.right) {
+        traversal(root.right);
       }
 
-      result.push(root.value)
+      result.push(root.value);
     }
-    
-    traversal(this.root)
 
-    return result
+    traversal(this.root);
+
+    return result;
   }
 
   deleteNode(value) {
@@ -171,35 +172,18 @@ class BinaryTree {
       }
     }
 
-    let root = this.root
-    removeNode(root, value)
-    
+    let root = this.root;
+    removeNode(root, value);
   }
 }
 
-const tree = new BinaryTree()
+const tree = new BinaryTree();
 
-const A = new Node(5)
-const B = new Node(4)
-const C = new Node(3)
-const D = new Node(2)
-const E = new Node(1)
-const F = new Node(6)
-const G = new Node(7)
-const H = new Node(8)
+const arr = [5, 9, 7, 3, 6, 2, 8, 10];
 
-tree.add(A)
-tree.add(B)
-tree.add(C)
-tree.add(D)
-tree.add(E)
-tree.add(F)
-tree.add(G)
-tree.add(H)
+for (let item of arr) {
+  tree.add(item);
+}
 
-tree.deleteNode(5)
-console.log(tree.root)
-
-
-
-
+tree.deleteNode(78);
+console.log(tree.root);
